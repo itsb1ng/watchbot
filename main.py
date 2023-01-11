90948e02df1af3d5ae43ab0a06227e6c703786ae
bing#0001 <65691264+itsb1ng@users.noreply.github.com>
2023-01-10 20:55:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+from discord.ext import commands, tasks
+import discord
+from discord.commands import Option
+from discord.ext.commands import cooldown, BucketType
+from twilio.rest import Client
+
+load_dotenv()
+
+token = os.environ['TOKEN']
+authKey = os.environ['AUTH']
+sid = os.environ['SID']
+phoneNumber = os.environ['PHONE_NUMBER']
+
+intents = discord.Intents.all()
+bing = commands.Bot(command_prefix='wb!', intents=intents)
+
+PINK_COLOR = 0xC98FFC
+
+def getInfo(call):
+    r = requests.get(call)
+    return r.json()
+
+
+client = Client(sid, authKey)
+@bing.event
+async def on_message(message):
+    send = False
+    if message.channel.id == 1057507992397938772:
+        send = True
+        channel = "Essential"
+        print("Essentials Hit")
+    if send is True:
+        message = client.messages.create(
+                body=f'There has been a hit in the {channel} hook',
+                messaging_service_sid='MG0f08ea8b441972497f6c63a7eabc1b1c',
+                to=phoneNumber
+            )
+        send = False
+
+bing.run(token)
